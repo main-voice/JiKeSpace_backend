13d83cf790c771689f47e6aa6b47ab738b5e087c
main-voice <voicemainid73@gmail.com>
2022-11-25 14:03:07 +0800

    init project

---
+++ src/main/java/com/tjsse/jikespace/JiKeSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.tjsse.jikespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiKeSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiKeSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tjsse/jikespace/JiKeSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjsse.jikespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiKeSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
